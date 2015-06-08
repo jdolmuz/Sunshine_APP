83a5405aa9635653f9b922d107ef93973b261112
jdolmuz <jdolmuz@gmail.com>
2015-06-07 21:03:40 -0400

    post
    sqllite

--- app/src/main/java/com/dolmuz/jean/sunshine_app/DetailActivity.java
+++ app/src/main/java/com/dolmuz/jean/sunshine_app/DetailActivity.java
@@ -16,7 +16,12 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//        setContentView(R.layout.activity_detail);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.action_bar_container, new DetailActivity())
+//                    .commit();
+//        }
     }
 
 
--- app/src/main/java/com/dolmuz/jean/sunshine_app/DetailActivityFragment.java
+++ app/src/main/java/com/dolmuz/jean/sunshine_app/DetailActivityFragment.java
@@ -3,7 +3,9 @@ package com.dolmuz.jean.sunshine_app;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,7 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ShareActionProvider;
+//import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import static android.support.v4.view.MenuItemCompat.getActionProvider;
@@ -35,8 +37,10 @@ public class DetailActivityFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Intent intent = getActivity().getIntent();
+
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent intent = getActivity().getIntent();
 
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
            // String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
@@ -49,14 +53,14 @@ public class DetailActivityFragment extends Fragment{
 
         @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-            super.onCreateOptionsMenu(menu, inflater);
+           // super.onCreateOptionsMenu(menu, inflater);
 
             inflater.inflate(R.menu.menu_detail, menu);
 
             MenuItem menuItem = menu.findItem(R.id.action_share);
 
             ShareActionProvider mShareActionProvider =
-                    (ShareActionProvider) menuItem.getActionProvider();
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
             //attach intent to this SAP
             if (mShareActionProvider != null) {
